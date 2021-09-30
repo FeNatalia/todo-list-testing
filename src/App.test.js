2eaa799f36bb4be07b2cde0345d267efaaf00cd3
Natalia Fedorova <nataliafedorova1989@gmail.com>
2021-09-30 18:41:10 +0200

    add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,7 @@
+// NPM Packages
 import { render, screen } from "@testing-library/react";
+
+// Project files
 import App from "./App";
 
 test("Expected to show the instructions screen if the todo list is empty", () => {
@@ -11,5 +14,25 @@ test("Expected to show the instructions screen if the todo list is empty", () =>
 
   // Act
   const textElement = screen.getByText(/welcome to todoapp/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test("Expected to show the normal screen if the todo list had an item/items", () => {
+  // Arrange
+  const item = {
+    id: 0,
+    name: "clean my desk",
+    isDone: false,
+  };
+  const data = [item];
+
+  Storage.prototype.getItem = jest.fn(() => JSON.stringify(data));
+
+  render(<App />);
+
+  // Act
+  const textElement = screen.getByText(/my todo list/i);
+
+  // Assert
   expect(textElement).toBeInTheDocument();
 });
